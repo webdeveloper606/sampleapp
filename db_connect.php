67273076e087218449ed49379fb7621609cf54a3
teju-web <71632961+teju-web@users.noreply.github.com>
2020-09-22 14:51:24 +0530

    first commit

---
+++ db_connect.php
@@ -0,0 +1,3 @@
+<?php 
+
+$conn= new mysqli('aa1txui64oih6ws.cjkjrvhh2dfo.ap-south-1.rds.amazonaws.com','root','Esolutions$143','aa1txui64oih6ws')or die("Could not connect to mysql".mysqli_error($con));
