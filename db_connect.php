df6d4733082ecf9aec8d84497e80195047885614
webdeveloper606 <71696097+webdeveloper606@users.noreply.github.com>
2020-09-23 17:41:44 +0530

    Update db_connect.php

--- db_connect.php
+++ db_connect.php
@@ -1,3 +1,3 @@
 <?php 
 
-$conn= new mysqli('aa154vcuta7ccvr.cjkjrvhh2dfo.ap-south-1.rds.amazonaws.com','root','Esolutions$143','aa154vcuta7ccvr')or die("Could not connect to mysql".mysqli_error($con));
+$conn= new mysqli('aa154vcuta7ccvr.cjkjrvhh2dfo.ap-south-1.rds.amazonaws.com','root','Esolutions$143','vote')or die("Could not connect to mysql".mysqli_error($con));
